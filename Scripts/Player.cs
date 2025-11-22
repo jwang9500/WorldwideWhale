176e28a89a1774c879609269f222e7b631bf8feb
jwang9500 <149388458+jwang9500@users.noreply.github.com>
2025-11-22 17:49:04 -0500

    Asset Update

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -27,7 +27,6 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Vector2.Zero;
-		GD.Print("Physics running");
 
 		if (Input.IsActionPressed("moveRight")) velocity.X += 1;
 		if (Input.IsActionPressed("moveLeft")) velocity.X -= 1;
