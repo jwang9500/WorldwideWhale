ebfcafd631fb7dc75ea222e032518d2b51ae7a68
jwang9500 <149388458+jwang9500@users.noreply.github.com>
2025-11-22 14:36:14 -0500

    Update Whale Sprite

--- Player.cs
+++ Player.cs
@@ -60,12 +60,7 @@ public partial class Player : Area2D
 			// See the note below about the following boolean assignment.
 			animatedSprite2D.FlipH = velocity.X < 0;
 		}
-		else if (velocity.Y != 0)
-		{
-			animatedSprite2D.Animation = "up";
-			animatedSprite2D.FlipV = velocity.Y > 0;
-		}
-
+		
 	}
 
 }
